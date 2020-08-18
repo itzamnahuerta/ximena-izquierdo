fc69761fd1c45d1388066b0d567bc41f654f16d8
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-18 16:35:17 -0400

    modified

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,32 +2,6 @@ import React, { Component } from 'react';
 import '../styles/Gallery.scss'
 import data from '../assets/data/data';
 
-
-
-const col1 = [];
-const col2 = [];
-const col3 = [];
-const col4 = [];
-const col5 = [];
-const col6 = [];
-const col7 = [];
-const col8 = [];
-const col9 = [];
-const col10 = [];
-const col11 = [];
-const col12 = [];
-const col13 = [];
-const col14 = [];
-const col15 = [];
-const col16 = [];
-const col17 = [];
-const col18 = [];
-const col19 = [];
-const col20 = [];
-const col21 = [];
-const col22 = [];
-const col23 = [];
-
 export default class Gallery extends Component {
   constructor() {
     super();
@@ -37,7 +11,7 @@ export default class Gallery extends Component {
     }
   }
 
-  
+
 
   render() {
     let renderPhotos = () => {
