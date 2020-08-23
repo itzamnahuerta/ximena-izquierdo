d062d61709467b2f3bac76784e0dea0047fa5421
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-22 23:27:40 -0400

    build directory

---
+++ build/precache-manifest.ce6466545dbb0c7d60416bb203e06693.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683d6858208062cf1306aad507a3e1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37517d540e4aaa4fae58",
+    "url": "/static/css/main.d821948e.chunk.css"
+  },
+  {
+    "revision": "b5189721129c1fbbbae6",
+    "url": "/static/js/2.72661225.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72661225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37517d540e4aaa4fae58",
+    "url": "/static/js/main.3a345cf8.chunk.js"
+  },
+  {
+    "revision": "2da695eeeb1e7e863336",
+    "url": "/static/js/runtime-main.c99ad947.js"
+  },
+  {
+    "revision": "db5edae5fe203454c7a09325da4c35c9",
+    "url": "/static/media/Continua-Regular.db5edae5.otf"
+  },
+  {
+    "revision": "fc95b23567b6e4337d356f5d5f405f4d",
+    "url": "/static/media/UniversNextPro-Regular.fc95b235.otf"
+  },
+  {
+    "revision": "66b64958d82f1aecf9e1ef8b247bcaee",
+    "url": "/static/media/WhyteInktrap-Medium.66b64958.otf"
+  },
+  {
+    "revision": "eab2d81fada411698083b3220f28c2d7",
+    "url": "/static/media/atlasgrotesk-regular.eab2d81f.ttf"
+  },
+  {
+    "revision": "b589fbe1ceaf0c48f7abd6664fbe5716",
+    "url": "/static/media/atlastypewriter-bold-reduced-webfont.b589fbe1.ttf"
+  },
+  {
+    "revision": "b7456a51363ad29bd67aafba0430f505",
+    "url": "/static/media/cigars-light-webfont.b7456a51.otf"
+  },
+  {
+    "revision": "668475a5df7dc6918c8ae735d32e5679",
+    "url": "/static/media/tiempos.668475a5.otf"
+  }
+]);
