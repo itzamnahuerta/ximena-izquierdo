0a6e5ec2fb598b3aea9ad19f788b863b80b76aca
Itzamna Huerta <_@itzamna-huerta.local>
2020-08-31 14:33:26 -0400

    moved console log into return jsx

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import '../styles/Photo.scss';
+
 class Photo extends Component {
   render() {
-    console.log("kust checking",this.props.data)
 
     const photoInfo = this.props.data;
     
-    return (
-      <div className="photo-container">
+    return (<div>
+
+      {this.props.showPhotoInfo &&  photoInfo !== {} && (
+        <div className="photo-container">
         <h2> {photoInfo.portrait} </h2>
         <h5> {photoInfo.location} </h5>
         <p> {photoInfo.personalNote} </p>
         <img src={photoInfo.imgUrl}/>
 
       </div>
+      )}
+      {console.log('rendering')}
+      </div>
+     
     )
   }
 }
