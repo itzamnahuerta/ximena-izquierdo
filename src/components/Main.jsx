52a358cd807f39ce747325be15caee23821ec04b
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-09 00:25:28 -0400

    removed heading to its own component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,50 +10,21 @@ class Main extends React.Component {
     super(props);
 
     this.state = {
-      isArtistStatementClicked: false,
-      currentTitle: "se que fue así porque estuve allí"
+      isPhotoGridClicked : false
     }
   }
 
-  componentDidMount(){
-    setInterval(() => {
-      if(this.state.currentTitle == "se que fue así porque estuve allí"){
-        this.setState({currentTitle:"xime izquierdo ugaz"})
-      }else if(this.state.currentTitle == "xime izquierdo ugaz"){
-        this.setState({currentTitle:"se que fue así porque estuve allí"})
-      }
-    }, 1000);
-  }
-
-  handleArtistStatement = () => {
-    this.setState({isArtistStatementClicked:!this.state.isArtistStatementClicked})
-  }
 
 
   render() {
-    if(this.state.isArtistStatementClicked === true) {
+    if(this.state.isPhotoGridClicked === true) {
       return (
-        <ArtistStatement
-            isArtistStatementClicked={this.state.isArtistStatementClicked}
-            handleArtistStatement={this.handleArtistStatement}
-          />
+          <div> WE GOOOOOOOD </div>
       )
     } else {
  
       return (
         <div>
-          <header className="gallery-header"> 
-              <div className="gallery-title">  
-               <h3 >{this.state.currentTitle}</h3 >
-              </div>
-              <h3 
-                className="artist-statement-header" 
-                onClick={this.handleArtistStatement}
-              > 
-                about
-              </h3>
-          </header>
-          
           <Gallery />
         </div>
       )
