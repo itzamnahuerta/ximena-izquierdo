e75b62360749da370561f047833ae7f7e0b60cd5
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-09 01:05:40 -0400

    -

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,14 +6,59 @@ import Gallery from './Gallery';
 // https://stackoverflow.com/questions/50490304/how-to-make-audio-autoplay-on-chrome // for autoplay audio
 
 class Main extends React.Component {
-  render() {
- 
-    return (
-      <div>
-        <Gallery />
-      </div>
-    )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isArtistStatementClicked: false,
+      currentTitle: "se que fue así porque estuve allí"
+    }
   }
+
+  componentDidMount(){
+    setInterval(() => {
+      if(this.state.currentTitle == "se que fue así porque estuve allí"){
+        this.setState({currentTitle:"xime izquierdo ugaz"})
+      }else if(this.state.currentTitle == "xime izquierdo ugaz"){
+        this.setState({currentTitle:"se que fue así porque estuve allí"})
+      }
+    }, 1000);
+  }
+
+  handleArtistStatement = () => {
+    this.setState({isArtistStatementClicked:!this.state.isArtistStatementClicked})
+  }
+
+
+  render() {
+    if(this.state.isArtistStatementClicked === true) {
+      return (
+        <ArtistStatement
+            isArtistStatementClicked={this.state.isArtistStatementClicked}
+            handleArtistStatement={this.handleArtistStatement}
+          />
+      )
+    } else {
+ 
+      return (
+        <div>
+          <header className="gallery-header"> 
+              <div className="gallery-title">  
+               <h3 >{this.state.currentTitle}</h3 >
+              </div>
+              <h3 
+                className="artist-statement-header" 
+                onClick={this.handleArtistStatement}
+              > 
+                about
+              </h3>
+          </header>
+          
+          <Gallery />
+        </div>
+      )
+    }
+    }
 }
 
 export default Main;
