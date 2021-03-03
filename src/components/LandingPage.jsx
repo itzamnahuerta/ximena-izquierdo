bd19e42bb0504979503de04baddaef95b9f71ca0
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2021-03-03 17:35:10 -0500

    updated 3 paragraph 1 sentence in eng & span

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
-import ReactImageAppear from 'react-image-appear';
 import '../styles/LandingPage.scss';
-
 import artistImg from '../assets/artist-img/xime_retrato.PNG'
+import {Link} from 'react-router-dom';
+
+// libraries
+import ReactImageAppear from 'react-image-appear';
+
+
 
  const LandingPage = (props) =>  {
   return (
     <div className="page-container">
-      <div class="marquee">
-        <div class="marquee__item">
+      <div className="marquee">
+        <div className="marquee__item">
           Estamos Bien: La Trienal 20/21
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Commissioned by El Museo del Barrio
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Curated by Rodrigo Moura, Susanna Temkin, and Elia Alba
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Estamos Bien: La Trienal 20/21
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Commissioned by El Museo del Barrio
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Curated by Rodrigo Moura, Susanna Temkin, and Elia Alba
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
         </div>
-        <div class="marquee__item">
+        <div className="marquee__item">
           Estamos Bien: La Trienal 20/21
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Commissioned by El Museo del Barrio
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Curated by Rodrigo Moura, Susanna Temkin, and Elia Alba
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Estamos Bien: La Trienal 20/21
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Commissioned by El Museo del Barrio
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
           Curated by Rodrigo Moura, Susanna Temkin, and Elia Alba
-          <span class="marquee__seperator">+++</span>
+          <span className="marquee__seperator">+++</span>
         </div>
       </div>
 
-      <div className="flex-container fade-in">
+      <div className="lp-flex-container fade-in">
         <div className="grid-main-section">
           <div className="title-item se">
             <h1> Se </h1> 
@@ -82,9 +86,11 @@ import artistImg from '../assets/artist-img/xime_retrato.PNG'
             </div>
       
             <div>
-              <button className="view-gallery" onClick={props.handleLandingPage} > 
-              <p className="opt-btn"> View Gallery</p>
-              </button>
+              <Link to="/home" >
+                <button className="view-gallery">
+                  <p className="opt-btn"> View Gallery</p>
+                </button>
+              </Link> 
             </div>
           </div>
 
@@ -98,7 +104,7 @@ import artistImg from '../assets/artist-img/xime_retrato.PNG'
             Hace casi 3 años, comencé a hacer retratos de mi familia elegida. Comenzó como un archivo para mí, para recordarme a mí misme los seres increíbles que me han rodeado, nutrido y amado de formas para las que a veces ni siquiera estaba preparade. Empecé a preguntar si podía tomar los retratos de mi familia. Verás, es una gran responsabilidad capturar un segundo de aquellxs que han sido tus hermanes y tus madres cuando todavía estabas tratando de averiguar qué significaba familia. 
             <br/> 
             <br/> 
-            La fotografía tiene una historia y un presente de ser utilizada como herramienta para la vigilancia de personas racializadas, y tenemos el deber de contravigilar. En un momento en el que la extracción y la muerte están tan presentes en nuestras comunidades, esta serie ha sido mi manera de documentar la cotidianidad de nuestras vidas y nuestra resiliencia, y para mostrar la belleza que veo en mis momentos más íntimos con todes mis hermanxs feroces, hermoses, Negres y Marronxs, Maricas y Trans. En los siguientes retratos escucharás y leerás las voces de todes en primera persona, se les pidió que respondieran a la pregunta, "¿cómo describirías a la persona en este retrato?"
+            La fotografía tiene una historia y un presente de ser utilizada como herramienta para la vigilancia de personas racializadas, y como nos recuerda mi amigue Alan Pelaez Lopez, tenemos el deber de contravigilar. En un momento en el que la extracción y la muerte están tan presentes en nuestras comunidades, esta serie ha sido mi manera de documentar la cotidianidad de nuestras vidas y nuestra resiliencia, y para mostrar la belleza que veo en mis momentos más íntimos con todes mis hermanxs feroces, hermoses, Negres y Marronxs, Maricas y Trans. En los siguientes retratos escucharás y leerás las voces de todes en primera persona, se les pidió que respondieran a la pregunta, "¿cómo describirías a la persona en este retrato?"
             <br/> 
             <br/> 
             Esta es una carta de amor para nosotres, para mi familia elegida como los universos que cada une es, en nuestra vida. Son nuestras vidas las que son sagradas, las formas en que nos vemos a nosotres mismxs, las formas en que nos permitimos ser. Veo cada retrato de esta serie como un retrato de mí misme también, porque estuve allí.
@@ -116,7 +122,7 @@ import artistImg from '../assets/artist-img/xime_retrato.PNG'
             Almost 3 years ago, I started taking portraits of my chosen family. It started as an archive for me, to remind myself of the incredible beings that have surrounded me, nurtured me and loved me in ways that I sometimes wasn’t even ready for. I started asking if I could take my fam’s portraits. It’s a big responsibility you see, to capture a second of those that have been your siblings and your mothers when you were still trying to figure out what family meant. 
             <br/>
             <br/>
-            Photography has a history and a present of being used as a tool for surveillance of Black and Brown people, and we have a duty to countersurveille. In a moment in which extraction and death is so present in our communities, this series has been my way to document the everydayness of our lives and our resilience, and to showcase the beauty that I see in my most intimate moments with all of my fierce, gorgeous, Black & Brown, queer and trans kin. In the following portraits you will hear and read the voices of everyone in first person, they were asked to respond to the prompt, “how would you describe the person in this portrait?”
+            Photography has a history and a present of being used as a tool for surveillance of Black and Brown people, and as my friend Alan Pelaez Lopez reminds us: we have a duty to countersurveille. In a moment in which extraction and death is so present in our communities, this series has been my way to document the everydayness of our lives and our resilience, and to showcase the beauty that I see in my most intimate moments with all of my fierce, gorgeous, Black & Brown, queer and trans kin. In the following portraits you will hear and read the voices of everyone in first person, they were asked to respond to the prompt, “how would you describe the person in this portrait?”
             <br/>
             <br/>
             This is a love letter to us, to my chosen family as the universes that they each are, in our lifetime. It is our lives that are sacred, the ways in which we see ourselves, and each other, the ways we allow ourselves to just be. I see every portrait in this series as a portrait of myself as well, because I was there. 
